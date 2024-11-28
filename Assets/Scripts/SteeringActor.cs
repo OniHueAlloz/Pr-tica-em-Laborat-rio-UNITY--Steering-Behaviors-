50b3596eef608f04ec8728a417800ed30fd78644
OniHueAlloz <marinhoj07@gmail.com>
2024-11-28 17:02:50 -0300

    Completo (Falta Escrever no Readme)

--- Assets/Scripts/SteeringActor.cs
+++ Assets/Scripts/SteeringActor.cs
@@ -1,8 +1,8 @@
 using UnityEngine.UI;
 using UnityEngine;
 
-enum Behavior { Idle, Seek, Evade }
-enum State { Idle, Arrive, Seek, Evade }
+enum Behavior { Idle, Seek, Evade, Flee, Pursuit, Cohesion, WallAvoid }
+enum State { Idle, Arrive, Seek, Evade, Flee, Pursuit, Cohesion, WallAvoid }
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class SteeringActor : MonoBehaviour
@@ -10,15 +10,19 @@ public class SteeringActor : MonoBehaviour
     [Header("Settings")]
     [SerializeField] Behavior behavior = Behavior.Seek;
     [SerializeField] Transform target = null;
+    [SerializeField] Transform wall = null;
     [SerializeField] float maxSpeed = 4f;
     [SerializeField, Range(0.1f, 0.99f)] float decelerationFactor = 0.75f;
     [SerializeField] float arriveRadius = 1.2f;
     [SerializeField] float stopRadius = 0.5f;
     [SerializeField] float evadeRadius = 5f;
+    [SerializeField] float fleeRadius = 1f;
+    [SerializeField] float stalkRadius = 2.5f;
 
     Text behaviorDisplay = null;
     Rigidbody2D physics;
     State state = State.Idle;
+    bool hasFled = false;
 
     void FixedUpdate()
     {
@@ -29,12 +33,23 @@ public class SteeringActor : MonoBehaviour
                 case Behavior.Idle: IdleBehavior(); break;
                 case Behavior.Seek: SeekBehavior(); break;
                 case Behavior.Evade: EvadeBehavior(); break;
+                case Behavior.Flee: FleeBehavior(); break;
+                case Behavior.Pursuit: PursuitBehavior(); break;
+                case Behavior.Cohesion: CohesionBehavior(); break;
+                case Behavior.WallAvoid: WallAvoidBehavior(); break;
             }
         }
 
         physics.velocity = Vector2.ClampMagnitude(physics.velocity, maxSpeed);
 
         behaviorDisplay.text = state.ToString().ToUpper();
+    }
+
+    void Awake()
+    {
+        physics = GetComponent<Rigidbody2D>();
+        physics.isKinematic = true;
+        behaviorDisplay = GetComponentInChildren<Text>();
     }
 
     void IdleBehavior()
@@ -102,11 +117,145 @@ public class SteeringActor : MonoBehaviour
         }
     }
 
-    void Awake()
+    void FleeBehavior()
     {
-        physics = GetComponent<Rigidbody2D>();
-        physics.isKinematic = true;
-        behaviorDisplay = GetComponentInChildren<Text>();
+        Vector2 delta = target.position - transform.position;
+        Vector2 steering = delta.normalized * maxSpeed - physics.velocity;
+        float distance = delta.magnitude;
+
+        if (!hasFled && distance > fleeRadius)
+        {
+            state = State.Idle;
+        }
+        else
+        {
+            hasFled = true;
+            state = State.Flee;
+        }
+
+        switch (state)
+        {
+            case State.Idle:
+                IdleBehavior();
+                break;
+            case State.Flee:
+                physics.velocity -= steering * Time.fixedDeltaTime;
+                break;
+        }
+
+    }
+
+    void PursuitBehavior()
+    {
+        Vector2 delta = target.position - transform.position;
+        Vector2 steering = delta.normalized * maxSpeed - physics.velocity;
+        float distance = delta.magnitude;
+
+        if (distance < stopRadius)
+        {
+            state = State.Idle;
+
+            if (transform.parent != target) 
+            {
+                transform.SetParent(target);
+                physics.velocity = Vector2.zero; 
+            }
+        }
+        else if (distance < arriveRadius)
+        {
+            state = State.Arrive;
+        }
+        else
+        {
+            state = State.Pursuit;
+        }
+
+        switch (state)
+        {
+            case State.Idle:
+                IdleBehavior();
+                break;
+            case State.Arrive:
+                var arriveFactor = 0.01f + (distance - stopRadius) / (arriveRadius - stopRadius);
+                physics.velocity += arriveFactor * steering * Time.fixedDeltaTime;
+                break;
+            case State.Pursuit:
+                physics.velocity += steering * Time.fixedDeltaTime;
+                break;
+        }
+    }
+
+    void CohesionBehavior()
+    {
+        Vector2 delta = target.position - transform.position;
+        Vector2 steering = delta.normalized * maxSpeed - physics.velocity;
+        float distance = delta.magnitude;
+
+        if (distance < stalkRadius)
+        {
+            state = State.Idle;
+        }
+        else if (distance < (stalkRadius+arriveRadius))
+        {
+            state = State.Arrive;
+        }
+        else
+        {
+            state = State.Cohesion;
+        }
+
+        switch (state)
+        {
+            case State.Idle:
+                IdleBehavior();
+                break;
+            case State.Arrive:
+                var arriveFactor = 0.01f + (distance - stalkRadius) / (arriveRadius);
+                physics.velocity += arriveFactor * steering * Time.fixedDeltaTime;
+                break;
+            case State.Cohesion:
+                physics.velocity += steering * Time.fixedDeltaTime;
+                break;
+        }
+    }
+
+    void WallAvoidBehavior()
+    {
+        Vector2 delta = target.position - transform.position;
+        Vector2 steering = delta.normalized * maxSpeed - physics.velocity;
+        float distance = delta.magnitude;
+
+        Vector2 alpha = wall.position - transform.position;
+        Vector2 alphasteering = alpha.normalized * maxSpeed - physics.velocity;
+        float walldistance = alpha.magnitude;
+
+        //wallavoid, evade, idle
+
+        if (walldistance > evadeRadius && distance > evadeRadius)
+        {
+            state = State.Idle;
+        }
+        else if(distance > evadeRadius)
+        {
+            state = State.WallAvoid;
+        }
+        else
+        {
+            state = State.Evade;
+        }
+
+        switch(state)
+        {
+            case State.Idle:
+                IdleBehavior();
+                break;
+            case State.Evade:
+                physics.velocity -= steering * Time.fixedDeltaTime;
+                break;
+            case State.WallAvoid:
+                physics.velocity -= alphasteering * Time.fixedDeltaTime;
+                break;
+        }
     }
 
     void OnDrawGizmos()
@@ -130,9 +279,37 @@ public class SteeringActor : MonoBehaviour
                 Gizmos.color = Color.red;
                 Gizmos.DrawWireSphere(transform.position, evadeRadius);
                 break;
+            case Behavior.Flee:
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(transform.position, fleeRadius);
+                break;
+            case Behavior.Pursuit:
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawWireSphere(transform.position, arriveRadius);
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawWireSphere(transform.position, stopRadius);
+                break;
+            case Behavior.Cohesion:
+                Gizmos.color = Color.Lerp(Color.red, Color.blue, 0.5f);
+                Gizmos.DrawWireSphere(transform.position, stalkRadius);
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(transform.position, (stalkRadius + arriveRadius));
+                break;
+            case Behavior.WallAvoid:
+                Gizmos.color = Color.Lerp(Color.red, Color.yellow, 0.5f);
+                Gizmos.DrawWireSphere(transform.position, evadeRadius);
+                break;
         }
 
         Gizmos.color = Color.gray;
         Gizmos.DrawLine(transform.position, target.position);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            wall = other.transform;
+        }
     }
 }
